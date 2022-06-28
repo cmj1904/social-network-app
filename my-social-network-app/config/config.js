81d5597ea31e865b271f8cce741e01176dee2da6
cmj1980 <dagoclmt@gmail.com>
2022-06-27 20:52:51 -0700

    deps added

---
+++ my-social-network-app/config/config.js
@@ -0,0 +1,7 @@
+const config = {
+ env: process.env.NODE_ENV || 'development',
+ port: process.env.PORT || 9000,
+ jwtSecret: process.env.JWT_SECRET || "ITS_my_secret_key"
+}
+
+export default config
